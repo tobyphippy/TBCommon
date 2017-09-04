2f68b7ab3fdb81908ea74bc42eb74b3ce57b5618
james <james@gmail.com>
2017-09-04 09:43:42 +0800

    first commit

---
+++ Common.h
@@ -0,0 +1,22 @@
+//
+//  Common.h
+//  TextVersion
+//
+//  Created by toby on 04/09/2017.
+//  Copyright © 2017 kg.self.edu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Common : NSObject
+
+/**
+ 比较两个版本号的大小
+ 
+ @param v1 第一个版本号
+ @param v2 第二个版本号
+ @return 版本号相等,返回0; v1小于v2,返回-1; 否则返回1.
+ */
++ (NSInteger)compareVersion:(NSString *)v1 to:(NSString *)v2;
+
+@end
